3242c173f8cfb4e7b813f38c0a2de972f7ee2bcc
aziz <azolzo778@gmail.com>
2022-12-14 11:24:21 +0200

    ...

--- lib/controller/favourit_controller.dart
+++ lib/controller/favourit_controller.dart
@@ -6,7 +6,6 @@ import 'package:get/get.dart';
 class FavouritController extends GetxController{
 
   var modelProduct=FullCategory(products: []).obs;
-
   addToFavourites({required Products product}){
     for (var item in modelProduct.value.products!) {
       if (item.favoriteCount ==1) {
