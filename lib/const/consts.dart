30e1a4490ac34c2d27988003028ba121e7bf7c2d
JamilREX <jamilhumidi@gmail.com>
2023-01-17 00:12:06 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/components/category_card.dart
    #	lib/controller/category_content_controller.dart
    #	lib/controller/home_controller.dart
    #	lib/view/category_content_view.dart
    #	lib/view/home_view.dart

--- lib/const/consts.dart
+++ lib/const/consts.dart
@@ -3,29 +3,7 @@
 class KConstants {
 
   static const String domain = 'https://api-card-store.technorex.net/';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   static const String whatsAppUrl = 'https://wa.me/qr/RCCAOIASEU2FA1';
   static const String telegramUrl= 'https://t.me/Azo778';
-
-
-
-
-
-
-
-
 
 }
